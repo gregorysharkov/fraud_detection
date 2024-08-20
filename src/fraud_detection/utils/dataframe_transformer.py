9f20c0ae379de48006fa48572a99c4aa5e0c1cac
Grigory Sharkov <gregorysharkov@gmail.com>
2024-08-20 08:48:50 +0800

    refactor: playing around with dataframe transformer

--- src/fraud_detection/utils/dataframe_transformer.py
+++ src/fraud_detection/utils/dataframe_transformer.py
@@ -1,10 +1,30 @@
+from typing import Any
+
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
-class DataFrameToListTransformer(BaseEstimator, TransformerMixin):
+class DataFrameTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, id_columns: list[str]) -> None:
+        self.id_columns = id_columns
+
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
-        return pd.DataFrame(X).apply(lambda row: row.tolist(), axis=1)
+        transformed_data = (
+            pd.DataFrame(X)
+            .apply(lambda row: _transform_row(row, self.id_columns), axis=1)
+            .apply(pd.Series)
+        )
+        transformed_data.columns = ["features"] + self.id_columns
+        return transformed_data
+
+
+def _transform_row(row, id_cols: list[str]) -> tuple[list, Any, Any]:
+    """Transforms a single row into a list"""
+    row = row.values.tolist()
+    features = row[:-len(id_cols)]
+    id_values = row[-len(id_cols):]
+    print(f"Features: {features}, ID values: {id_values}\nrow: {row}")
+    return (features, *id_values)
--- src/fraud_detection/utils/row_preprocessor.py
+++ src/fraud_detection/utils/row_preprocessor.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import FunctionTransformer, StandardScaler
 
 from fraud_detection.utils.fast_text_encoder import FastTextEncoder
 from fraud_detection.utils.concatenate_transformer import ConcatenateTransformer
-from fraud_detection.utils.dataframe_transformer import DataFrameToListTransformer
+from fraud_detection.utils.dataframe_transformer import DataFrameTransformer
 from fraud_detection.utils.row_combination_utils import combine_features, select_single_label
 
 
@@ -15,6 +15,8 @@ FAST_MODEL_SETTINGS = {
     "min_count": 3,
     "workers": 4,
 }
+
+ID_COLS = ["cc_num", "window_id", "label"]
 
 
 def get_row_preprocessor(
@@ -34,7 +36,35 @@ def get_row_preprocessor(
         A pipeline that transforms a single row into an array after embedding.
     """
 
-    preprocessors = [
+    preprocessors = _generate_encoder_pipeline(cat_columns, embedder_settings)
+    numeric_pipelines = _generate_numeric_pipeline(num_columns)
+    preprocessors.extend(numeric_pipelines)
+    id_pipelines = _generate_id_columns_pipeline(ID_COLS)
+    preprocessors.extend(id_pipelines)
+
+    feature_union = FeatureUnion(transformer_list=preprocessors, n_jobs=1, verbose=True, verbose_feature_names_out=True)
+
+    return Pipeline(
+        steps=[
+            ("combined_embeddings", feature_union),
+            ("concatenate_features", ConcatenateTransformer()),
+            ("convert_to_dataframe", DataFrameTransformer(ID_COLS)),
+        ],
+    )
+
+
+def _generate_encoder_pipeline(cat_columns: list[str], embedder_settings: dict) -> list[tuple[str, Pipeline]]:
+    """
+    Generates a single encoder pipeline for a specific column.
+
+    Args:
+        cat_columns: List of columns to be treated as categorical columns.
+        embedder_settings: Settings for the TextEmbedder.
+
+    Returns:
+        A tuple containing the column name and the encoder pipeline.
+    """
+    return [
         (
             f"encoder_pipeline_{x}",
             Pipeline(
@@ -46,58 +76,50 @@ def get_row_preprocessor(
         for x in cat_columns
     ]
 
-    preprocessors.extend(
-        [
-            (
-                f"numeric_pipeline_{x}",
-                Pipeline(
-                    [
-                        (f"selector_{x}", FunctionTransformer(lambda X: X[[x]], validate=False)),
-                        (f"scaler_{x}", StandardScaler()),
-                        (f"imputer_{x}", SimpleImputer(strategy="mean")),
-                    ]
-                )
+
+def _generate_numeric_pipeline(num_columns: list[str]) -> list[tuple[str, Pipeline]]:
+    """
+    Generates a single numeric pipeline for a specific column.
+
+    Args:
+        num_columns: List of columns to be treated as numerical columns.
+
+    Returns:
+        A tuple containing the column name and the numeric pipeline.
+    """
+    return [
+        (
+            f"numeric_pipeline_{x}",
+            Pipeline(
+                [
+                    (f"selector_{x}", FunctionTransformer(lambda X: X[[x]], validate=False)),
+                    (f"scaler_{x}", StandardScaler()),
+                    (f"imputer_{x}", SimpleImputer(strategy="mean")),
+                ]
             )
-            for x in num_columns
-        ]
-    )
-    feature_union = FeatureUnion(transformer_list=preprocessors, n_jobs=1, verbose=True, verbose_feature_names_out=True)
-
-    return Pipeline(
-        steps=[
-            ("combined_embeddings", feature_union),
-            ("concatenate_features", ConcatenateTransformer()),
-            ("convert_to_dataframe", DataFrameToListTransformer()),
-        ],
-    )
+        )
+        for x in num_columns
+    ]
 
 
-# def get_combined_row_preprocessor(
-#     cat_columns: list[str],
-#     num_columns: list[str],
-#     embedder_settings: dict,
-# ) -> Pipeline:
-#     """
-#     Generates a pipeline to preprocess the incoming data and combine features and labels.
+def _generate_id_columns_pipeline(id_cols: list[str]) -> list[tuple[str, Pipeline]]:
+    """
+    Generates a single pipeline for ID columns.
 
-#     Args:
-#         cat_columns: List of columns to be treated as categorical columns.
-#         num_columns: List of columns to be treated as numerical columns.
-#         embedder_settings: Settings for the TextEmbedder.
+    Args:
+        id_cols: List of columns to be treated as ID columns.
 
-#     Returns:
-#         A pipeline that transforms a single row into an array after embedding and combining features and labels.
-#     """
-
-
-#     row_encoder = get_row_preprocessor(cat_columns, num_columns, embedder_settings)
-#     feature_aggregator = FunctionTransformer(
-#         func=combine_features,
-#         validate=False,
-#     )
-
-#     return Pipeline(
-#         steps=[
-#             ("row_preprocessor", row_encoder),
-#         ]
-#     )
+    Returns:
+        A tuple containing the column name and the ID pipeline.
+    """
+    return [
+        (
+            f"id_pipeline_{x}",
+            Pipeline(
+                [
+                    (f"selector_{x}", FunctionTransformer(lambda X: X[[x]], validate=False)),
+                ]
+            )
+        )
+        for x in id_cols
+    ]
